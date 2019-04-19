b5871e7e0aa566f094c1439768e967b3bf7cd6f7
tbenis <will.jenifer@yahoo.com>
2019-04-19 01:22:47 -0400

    corrected minor logic err, added bootsrtrap for better look and feel.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@ function pickRandomQuote(){
     return Math.floor(Math.random() * quotes.length) +1;
 }
 function generateQuote(){
+    var selectdQ = pickRandomQuote();
     for (var i = 0; i < quotes.length; i++) {
-        document.getElementById('quote').innerHTML = quotes[pickRandomQuote()].quote;
-        document.getElementById('author').innerHTML = quotes[pickRandomQuote()].author;
+        document.getElementById('quote').innerHTML = quotes[selectdQ].quote;
+        document.getElementById('author').innerHTML = "- " + quotes[selectdQ].author;
     }
 }
